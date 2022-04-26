20623c7823532415d1cc7faa9a578b7aafd279cb
prakash19csu214 <prakashsingh112000@gmail.com>
2022-04-26 13:30:31 +0530

    Search Layout

--- ConvoX/client/src/components/ChannelListContainer.jsx
+++ ConvoX/client/src/components/ChannelListContainer.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 // import { ChannelList, useChatContext } from "stream-chat-react";
 // import Cookies from 'universal-cookie';
-// import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
 
-import HospitalIcon from '../assets/images/hospital.png';
+import ChatIcon from '../assets/images/hospital.png';
 import LogoutIcon from '../assets/images/logout.png';
 
 const SideBar = () => (
   <div className="channel-list__sidebar">
       <div className="channel-list__sidebar__icon1">
           <div className="icon1__inner">
-              <img src={HospitalIcon} alt="Hospital" width="30" />
+              <img src={ChatIcon} alt="Icon" width="30" />
           </div>
       </div>
       <div className="channel-list__sidebar__icon2">
@@ -21,10 +21,21 @@ const SideBar = () => (
   </div>
 );
 
+const CompanyHeader = () => (
+  <div className="channel-list__header">
+      <p className="channel-list__header__text">ConvoX</p>
+  </div>
+);
+
+
 const ChannelListContainer = () => {
   return (
     <>
     <SideBar />
+    <div className="channel-list__list__wrapper">
+      <CompanyHeader />
+      <ChannelSearch />
+    </div>
     </>
   )
 }
