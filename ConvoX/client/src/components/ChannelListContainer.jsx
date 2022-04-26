96f2f7553b1b3e10f18dfc032e9589adb113820d
prakash19csu214 <prakashsingh112000@gmail.com>
2022-04-26 10:08:35 +0530

    sidebar

--- ConvoX/client/src/components/ChannelListContainer.jsx
+++ ConvoX/client/src/components/ChannelListContainer.jsx
@@ -3,23 +3,24 @@ import React from 'react'
 // import Cookies from 'universal-cookie';
 // import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
 
-import '../assets/images/hospital.png';
-import '../assets/images/logout.png';
+import HospitalIcon from '../assets/images/hospital.png';
+import LogoutIcon from '../assets/images/logout.png';
 
-const SideBar = () =>{
+const SideBar = () => (
   <div className="channel-list__sidebar">
-    <div className="channel-list__sidebar__icon1">
-      <div className="icon1__inner">
-        <img src='../assets/images/hospital.png' alt="Hospital" width="30px"/>
+      <div className="channel-list__sidebar__icon1">
+          <div className="icon1__inner">
+              <img src={HospitalIcon} alt="Hospital" width="30" />
+          </div>
       </div>
-    </div>
-    <div className="channel-list__sidebar__icon2">
-      <div className="icon1__inner">
-        <img src='../assets/images/logout.png' alt="Logout" width="30px"/>
+      <div className="channel-list__sidebar__icon2">
+          <div className="icon1__inner">
+              <img src={LogoutIcon} alt="Logout" width="30" />
+          </div>
       </div>
-    </div>
   </div>
-}
+);
+
 const ChannelListContainer = () => {
   return (
     <>
